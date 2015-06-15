53e2eae3efb8a7ac5dcc2b9660296bc5c3fc5905
samael <samael.65535@gmail.com>
2015-06-15 22:37:38 +0800

    Serving Static Content

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,17 +1,11 @@
 #!/usr/bin/env node
 var parseArgs = require('minimist')(process.argv.slice(2));
-var createMiniHarp = require("../index")
-    , app = createMiniHarp();
-var port = parseArgs['port'] || 4000;
 
+var createMiniHarp = require("../index")
+var port = parseArgs['port'] || 4000;
+var root = parseArgs._[0];
+var app = createMiniHarp(root);
 console.log("Starting mini-harp on http://localhost:" + port);
 
-app.use(function(request,response,next) {
-    if (request.url == "/current_time") {
-        response.end((new Date()).toISOString());
-    } else {
-        response.end("Cannot Get " +  request.url);
-    }
-});
 
 app.listen(port);
--- index.js
+++ index.js
@@ -2,8 +2,19 @@
  * Created by samael on 15-6-15.
  */
 
+var serveStatic = require('serve-static');
 var connect = require('connect');
-module.exports = function() {
-    return connect();
+
+module.exports = function(root) {
+    var app = connect();
+    //app.use(function(request,response,next) {
+    //    if (request.url == "/current_time") {
+    //        response.end((new Date()).toISOString());
+    //    } else {
+    //        response.end("Cannot Get " +  request.url);
+    //    }
+    //});
+    app.use(serveStatic(root));
+    return app;
 };
 
