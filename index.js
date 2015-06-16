a47fcb982e679d405ae0df58104028e2fad33b9a
samael <samael.65535@gmail.com>
2015-06-17 00:07:42 +0800

    Should not respond to .jade or .less

--- index.js
+++ index.js
@@ -2,15 +2,24 @@
  * Created by samael on 15-6-15.
  */
 
-var serveStatic = require('serve-static');
+//var serveStatic = require('serve-static');
 var connect = require('connect');
-var makeJade = require('./lib/processor/jade');
-var makeLess = require('./lib/processor/less');
+var path = require('path');
 module.exports = function(root) {
+    var makeJade = require('./lib/processor/jade')(root);
+    var makeLess = require('./lib/processor/less')(root);
     var app = connect();
-    app.use(makeJade(root));
-    //app.use(makeLess(root));
-    //app.use(serveStatic(root));
+    app.use(function(req, res, next) {
+        var extname= path.extname(req.url)
+        if (extname == '.html') {
+            makeJade(req, res, next);
+        } else if (extname == '.css') {
+            makeLess(req, res, next);
+        } else {
+            next();
+        }
+    });
+
     return app;
 };
 
